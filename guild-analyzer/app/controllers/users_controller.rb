03b531f595ce69e4ad0957d109d179c0bb5693af
Claire <email": null>
2017-02-23 16:03:55 -0800

    adds a concern for session and included in application controller and creates a new user function

--- guild-analyzer/app/controllers/users_controller.rb
+++ guild-analyzer/app/controllers/users_controller.rb
@@ -5,6 +5,22 @@ class UsersController < ApplicationController
     render :new
   end
 
+  def create
+    @user = User.new(user_params)
+    @user.password = user_params[:password_digest]
+    @user.timer_count = 0
+      if @user.save
+        session_login @user
+        redirect_to root_url
+      else
+        @user.errors.full_messages
+        render :new
+      end
+  end
 
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :gw2_character_id, :password_digest, :encrypted_password)
+  end
 
 end
