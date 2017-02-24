242dd40f8c1205dd78af16cd1a3e2d0cfb26992f
Claire <email": null>
2017-02-24 13:44:15 -0800

    adds a session helper for the view pages

---
+++ guild-analyzer/app/helpers/session_helper.rb
@@ -0,0 +1,21 @@
+module SessionHelper
+  def session_logged_in?
+    !session[:current_user_id].nil?
+  end
+
+  def session_user
+    begin
+      @current_user ||= User.find(session[:current_user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      session.clear
+      @current_user = nil
+    end
+  end
+
+  def session_logout
+    session.clear
+  end
+
+  def session_login user
+    session[:current_user_id] = user.id
+  end
