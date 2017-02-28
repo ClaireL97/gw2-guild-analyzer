f60e18ab31610fcbf1ebd3f8442a4c48f72a71e4
Claire C. Lindblom <claire.lindblom@gmail.com>
2017-02-27 16:43:22 -0800

    adds a guild homepage and sets rroot to the index

--- guild-analyzer/config/routes.rb
+++ guild-analyzer/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :login, :only => [:create, :new, :destroy]
 
-
+  root 'guilds#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
